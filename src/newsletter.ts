424451dbefd196cacc995b84ae83fd9f0c73083e
Damian Thomas <dimitrisonic@gmail.com>
2024-09-16 11:11:14 -0400

    rss feed -> sendgrid pipeline functions written

--- src/newsletter.ts
+++ src/newsletter.ts
@@ -1,17 +1,67 @@
 import { render } from "@react-email/components";
-import { getLatestIssue, loadSendgridApi } from "./utils";
-import { prompt } from "./utils";
+import sendgrid from "@sendgrid/mail";
+import dayjs from "dayjs";
+import "dotenv/config";
+import Parser from 'rss-parser';
 import NewIssue from "../emails/NewIssue";
+import { Issue, type IssueEmail } from "./types";
+import Confirmation from "../emails/Confirmation";
 
 const testTarget = "dimitrisonic@gmail.com";
+type FeedItem = {
+	"content:encoded": string,
+	"content:encodedSnippet": string,
+	description: string
+};
+
+export function loadSendgridApi() {
+	const key = process.env["SENDGRID_API_KEY"];
+	if (!key || key.length === 0) {
+		throw new Error("Sendgrid API key missing or malformed.");
+	}
+
+	sendgrid.setApiKey(key);
+	return sendgrid;
+}
+
+export async function getIssues(): Promise<IssueEmail[]> {
+	const parser: Parser<Record<string, any>, FeedItem> = new Parser({
+		customFields: {
+			item: ["content:encoded", "content:encodedSnippet"]
+		}
+	});
+	const feed = await parser.parseURL('https://fromthesuperhighway.com/rss.xml');
+	const emails: IssueEmail[] = [];
+	feed.items.forEach(item => {
+		if (!item.title || !item.link) { return; }
+		const data: Issue = {
+			title: item.title,
+			description: item.description,
+			publishDate: item.pubDate ?? new Date("1970"),
+			tags: item.categories
+		}
+
+		const slug = item.link.split("/").at(-1);
+		if (!slug) { return; }
+		const body = item['content:encoded'];
+		emails.push({ data, slug, body } as IssueEmail);
+	});
+
+	return emails;
+}
+
+export async function getLatestIssue(): Promise<IssueEmail> {
+	const sortedIssues = (await getIssues()).sort((
+		{ data: { publishDate: date1 } },
+		{ data: { publishDate: date2 } }
+	) => dayjs(date1).diff(date2));
+
+	return sortedIssues[0];
+}
+
 async function sendLatestIssue(to: string): Promise<unknown> {
-	const { issue, count } = await getLatestIssue();
-	console.log(`Preparing send for Issue #${count}: ${issue.slug}`);
-	console.log(JSON.stringify(issue.data, null, 4))
-	// const result = prompt("Is this the issue you want to send? (y/N) | ")
-	// if (result !== "y") {
-	// 	return "Aborted send."
-	// }
+	const issue = await getLatestIssue();
+	console.log(`Preparing send for Issue "${issue.data.title}"`);
 
 	console.log("Building HTML render...")
 	const html = await render(NewIssue({ issue }), { pretty: true });
@@ -28,8 +78,40 @@ async function sendLatestIssue(to: string): Promise<unknown> {
 		from: "newsletter@fromthesuperhighway.com",
 		to,
 		subject: issue.data.title,
-		html
+		html,
+		asm: {
+			groupId: 26518,
+			groupsToDisplay: [26518]
+		}
 	});
 
-	return "Email sent to UNIMPLEMENTED.";
+	return `Email sent to ${to}.`;
+}
+
+async function sendConfirmationEmail(to: string): Promise<unknown> {
+	const issue = await getLatestIssue();
+	console.log(`Preparing send for confirmation email for "${to}"`);
+
+	console.log("Building HTML render...")
+	const html = await render(Confirmation(), { pretty: true });
+	console.log("Render complete.")
+	console.log("Building plain text render...")
+	const plain = await render(Confirmation(), { plainText: true });
+	console.log("Render complete.")
+
+	console.log("Loading Sendgrid API...");
+	const sendgrid = loadSendgridApi();
+
+	await sendgrid.send({
+		from: "newsletter@fromthesuperhighway.com",
+		to,
+		subject: issue.data.title,
+		html,
+		asm: {
+			groupId: 26518,
+			groupsToDisplay: [26518]
+		}
+	});
+
+	return `Email sent to ${to}.`;
 }
